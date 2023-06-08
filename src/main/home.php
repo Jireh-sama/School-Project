985d4ea85d8d7fc010c2317eb58b8b0a7eb09716
Jireh-sama <jirehtumbagahan@gmail.com>
2023-06-08 19:00:54 +0800

    minor updates on shop

--- src/main/home.php
+++ src/main/home.php
@@ -1,39 +1,41 @@
 <?php
-    require 'config.php';
-    if (!empty($_SESSION["id"])) {
-        $id = $_SESSION["id"];
-        $result = mysqli_query($conn, "SELECT * FROM customer_data WHERE id = $id");
-        $row = mysqli_fetch_assoc($result);
-    }
-    else {
-        header("Location: Login.php");
-    }
+require 'conn.php';
+if (!empty($_SESSION["id"])) {
+  $id = $_SESSION["id"];
+  $result = mysqli_query($conn, "SELECT * FROM customer_data WHERE id = $id");
+  $row = mysqli_fetch_assoc($result);
+} else {
+  header("Location: Login.php");
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../style/css/style-home.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../style/css/style-home.css">
 </head>
+
 <body>
-    <main>
-        <header class="navbar">
-            <section class="navbar-section">
-                <h2>Logo</h2>
-            </section>
-            <section class="navbar-section navlinks">
-                <p>Home</p>
-                <p>Shop</p>
-                <p>About</p>
-            </section>
-            <section class="navbar-section">
-                <p> <?php echo$row['username']; ?> </p>
-            </section>
-        </header>
-    </main>
+  <main>
+    <header class="navbar">
+      <section class="navbar-section">
+        <h2>Logo</h2>
+      </section>
+      <section class="navbar-section navlinks">
+        <p>Home</p>
+        <p>Shop</p>
+        <p>About</p>
+      </section>
+      <section class="navbar-section">
+        <p> <?php echo $row['username']; ?> </p>
+      </section>
+    </header>
+  </main>
 </body>
+
 </html>
--- src/main/login.php
+++ src/main/login.php
@@ -1,75 +1,73 @@
 <?php
-    require 'config.php';
+require 'conn.php';
 
-    // LOGIN START HERE
-    if (isset($_POST['submit'])) {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+// LOGIN START HERE
+if (isset($_POST['submit'])) {
+  $email = $_POST['email'];
+  $password = $_POST['password'];
 
-        // Check for duplicates
-        $result = mysqli_query($conn, "SELECT * FROM customer_data WHERE email = '$email'");
-        $row = mysqli_fetch_assoc($result);
+  // Check for duplicates
+  $result = mysqli_query($conn, "SELECT * FROM customer_data WHERE email = '$email'");
+  $row = mysqli_fetch_assoc($result);
 
-        if (mysqli_num_rows($result) > 0) {
-            if ($password == $row['password']) {
-                $_SESSION['login'] = true;
-                $_SESSION['id'] = $row['id'];
-                header('location: home.php');
-            }
-            else {
-                // Write codes here for password not match
-            }
-        }
-        else {
-            echo"
+  if (mysqli_num_rows($result) > 0) {
+    if ($password == $row['password']) {
+      $_SESSION['login'] = true;
+      $_SESSION['id'] = $row['id'];
+      header('location: home.php');
+    } else {
+      // Write codes here for password not match
+    }
+  } else {
+    echo "
                 <script></script>
             ";
-        }
-       
-        
-    }
+  }
+}
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../style/css/style-login-register.css">
-    <link rel="stylesheet" href="../style/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="../../assets/fontawesome/css/all.min.css" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../style/css/style-login-register.css">
+  <link rel="stylesheet" href="../style/bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" href="../../assets/fontawesome/css/all.min.css" />
 </head>
+
 <body>
-    <main class="login-register-main">
-        <div class="form-container">
-            <header class="login-register-header">
-                <h1>Login</h1>
-            </header>
-            <form class="input-field" action="#" method="post">
-    
-                <section class="input-field-row">
-                    <label for="email">Email</label>
-                    <input class="form-control-main" id="email"  type="text" placeholder="Enter Email" name="email">
-                </section>
+  <main class="login-register-main">
+    <div class="form-container">
+      <header class="login-register-header">
+        <h1>Login</h1>
+      </header>
+      <form class="input-field" action="#" method="post">
 
-                <section class="input-field-row">
-                    <label for="password">Password</label>
-                    <input class="form-control-main" id="password"  type="password" placeholder="Enter Password" name="password">
-                </section>
+        <section class="input-field-row">
+          <label for="email">Email</label>
+          <input class="form-control-main" id="email" type="text" placeholder="Enter Email" name="email">
+        </section>
 
-                <input class="btn btn-primary" type="submit" value="Register" name="submit">
-            </form>
+        <section class="input-field-row">
+          <label for="password">Password</label>
+          <input class="form-control-main" id="password" type="password" placeholder="Enter Password" name="password">
+        </section>
 
-            <footer>
-                <p>Don't have an account Yet? <a href="register.php">Signup</a></p>
-            </footer>    
+        <input class="btn btn-primary" type="submit" value="Register" name="submit">
+      </form>
 
-        </div>
+      <footer>
+        <p>Don't have an account Yet? <a href="register.php">Signup</a></p>
+      </footer>
 
-    </main>
-    <script src="script.js"></script>
+    </div>
+
+  </main>
+  <script src="script.js"></script>
 </body>
+
 </html>
