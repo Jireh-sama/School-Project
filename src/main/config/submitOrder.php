481fbb068f6dd6f91b9ca618eca0d4b6484e4972
Jireh-sama <jirehtumbagahan@gmail.com>
2023-06-18 21:29:46 +0800

    Added Complete Order Function

--- src/main/config/submitOrder.php
+++ src/main/config/submitOrder.php
@@ -4,13 +4,9 @@
     if (isset($_POST['id']) && isset($_POST['quantity'])) {
         $itemId = $_POST['id'];
         $itemQuantity = $_POST['quantity'];
-        
-
-        // Compute the total price
         $itemQuery = mysqli_query($conn, "SELECT * FROM item_inventory WHERE id=$itemId");
         $item = mysqli_fetch_assoc($itemQuery);
         $total = $item['price'] * $itemQuantity;
-
         $setOrderQuery = mysqli_query($conn, "INSERT INTO order_receipt(item_id, item_quantity, total) VALUES('$itemId', '$itemQuantity', '$total')");
     }else {
         echo ('Variable Item or Quantity Not Found');
