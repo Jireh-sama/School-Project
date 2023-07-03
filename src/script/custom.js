705ee455a090a2165a05dd9f2622c3d1f91cad94
Jireh-sama <jirehtumbagahan@gmail.com>
2023-07-03 12:18:12 +0800

    replaced timeout functions with promises

--- src/script/custom.js
+++ src/script/custom.js
@@ -12,31 +12,46 @@ function setSelectedItem(id) {
     console.log(this.responseText);
   };
   xhr.send(myid);
-
-  setTimeout(() => {
+  let myPromise = new Promise(function (myResolve, myReject) {
+    // "Producing Code" (May take some time)
     const xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", "./functions/getSelectedItem.php", true);
-    xmlhttp.onload = async function(){
-      if(this.status == 200){
+    xmlhttp.onload = function () {
+      if (this.status == 200) {
         const stringFromPhp = this.responseText;
         const splittedString = stringFromPhp.split(",");
-        document.querySelector(".btn-set-order").id = splittedString[0];
-        document.getElementById("item-name").innerHTML = splittedString[1];
-        document.getElementById("item-price").innerHTML = splittedString[2] + " Php";+
-        console.log(this.responseText);
+        const itemName = document.getElementById("item-name").innerHTML = splittedString[1];
+        const itemPrice = document.getElementById("item-price").innerHTML = splittedString[2] + " Php";
+        const btnID = document.querySelector(".btn-set-order").id = splittedString[0];
+        console.log('Setting the Modal');
+        if(this.responseText){
+          myResolve(); // when successful
+        }else {
+          myReject(); // when error
+        }
       } else {
-        alert('Something went wrong in setting the Modal');
+        alert("Something went wrong in setting the Modal");
       }
-    }
+    };
     xmlhttp.send();
-    modal.showModal(); // OPEN MODAL
-  }, 1500);
+    
+  });
+  // "Consuming Code" (Must wait for a fulfilled Promise)
+  myPromise.then(
+    function (value) {
+      modal.showModal();
+      console.log('Promise is Resolved');
+    },
+    function (error) {
+      console.error('Error Promise not Resolved');
+    }
+  );
 }
 
 function closeModal() {
   modal.close();
   // Clear quantity value on close
-  document.querySelector(".item-quantity").value = ""; 
+  document.querySelector(".item-quantity").value = "";
 }
 function submitOrder(id) {
   const quantity = parseInt(document.querySelector(".item-quantity").value);
@@ -54,15 +69,15 @@ function submitOrder(id) {
     const xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
-        numOrder = parseInt(this.responseText)
+        numOrder = parseInt(this.responseText);
         if (numOrder >= 5) {
-          alert('Currently The Maximum Order Limit is 5');
-        }else {
+          alert("Currently The Maximum Order Limit is 5");
+        } else {
           xhr.send(myid);
           setTimeout(() => {
             modal.close();
             // Clear Quantity Value on submit
-            document.querySelector(".item-quantity").value = ""; 
+            document.querySelector(".item-quantity").value = "";
           }, 300);
         }
       }
